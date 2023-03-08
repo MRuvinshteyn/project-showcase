04c9667fced1568b70db3e880338fac3e3891c01
Michael Ruvinshteyn <michael.ruvinshteyn@gmail.com>
2023-03-07 16:27:38 -0800

    udpate prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to my Project Showcase!
-        </p>
+        <p>Welcome to my Project Showcase!</p>
         <ul>
           <li>
-            <a href='https://github.com/MRuvinshteyn/bounce-2d'
-              className='App-link'
-              rel='noreferrer'
-              target='_blank'>
+            <a
+              href="https://github.com/MRuvinshteyn/bounce-2d"
+              className="App-link"
+              rel="noreferrer"
+              target="_blank"
+            >
               Bounce2D - a game to test your geometric prowess
             </a>
           </li>
         </ul>
       </header>
-    </div >
+    </div>
   );
 }
 
